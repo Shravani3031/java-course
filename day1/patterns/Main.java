62a9db5ae30d8c376aff5aa7562694ef1269aa39
Shravani3031 <shravanisawant322@gmail.com>
2024-07-07 13:32:27 -0700

    full

---
+++ day1/patterns/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]) {
+        for(int j=0; j<6; j++){
+            for(int i=0; i<5; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
