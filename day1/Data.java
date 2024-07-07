a61437d6aec4b58d6436c477ae13d7c928369937
Shravani3031 <shravanisawant322@gmail.com>
2024-07-07 12:37:57 -0700

    day1 half

---
+++ day1/Data.java
@@ -0,0 +1,18 @@
+public class Data{
+    public static void main(String args[]) {
+    System.out.println("datatypes.java");
+    
+    int integerNumber = 4;
+    float decimalNumber = 5.3;
+    char character = '%';
+    String name = "Shravani";
+
+    System.out.println(integerNumber);
+    System.out.println(decimalNumber);
+    System.out.println(character);
+    System.out.println(name);
+
+
+
+    }
+}
---
+++ day1/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String args[]) {
+    System.out.println("basicCalaculators");
+    int a = 5;
+    int b = 3;
+    
+    int addition = a+b;
+    int subtraction = a-b;
+    int multiplication = a*b;
+    int division = a/b;
+    System.out.println(addition);
+    System.out.println(subtraction);
+    System.out.println(multiplication);
+    System.out.println(division);
+    
+    }
+}
