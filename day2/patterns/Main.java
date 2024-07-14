ea4eb01b01ddceacf1ce19951eec5f0e64c2b4a9
Shravani3031 <shravanisawant322@gmail.com>
2024-07-14 13:25:31 -0700

    day 2

---
+++ day2/patterns/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public int add(int a, int b){
+        int sum=a+b;
+        return sum;
+    }
+    
+    public static void main(String args[]){
+        Main object=new Main();
+        int result=object.add(5,10);
+        System.out.println(result);
+        
+            
+            
+       
+
+    }
+    
+}
+
+    
---
+++ day2/patterns/NumberPyramid.java
@@ -0,0 +1,17 @@
+class NumberPyramid {
+    
+    public static void main(String[] args) {
+        int n =6;
+    for(int i=0; i<n; i++){
+        
+        for(int j=0; j<i+1 ; j++){
+            System.out.print(j+1);
+            
+            
+            
+        }
+        System.out.println();
+    }  
+    
+}
+}
